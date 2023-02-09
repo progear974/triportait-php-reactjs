1564f5c8c0f45529f155f0f652a353d37b4dc559
Bax France <baxfrance@hotmail.fr>
2023-02-09 22:53:02 +0100

    update

--- src/Command/ZipImageCommand.php
+++ src/Command/ZipImageCommand.php
@@ -111,7 +111,7 @@ class ZipImageCommand extends Command
                 $singles_to_copy = implode(" ", $singles_filenames);
                 $process = Process::fromShellCommandline("cp {$singles_to_copy} {$this->triportraitTreeService->getPublicImagesFolderPath()}", timeout: null);
                 $process->mustRun(null);
-                array_map(array($this, 'resizeSingle'), $singles_filenames, array_fill(0, sizeof($singles_filenames), 1748), array_fill(0, sizeof($singles_filenames), 2402));
+                //array_map(array($this, 'resizeSingle'), $singles_filenames, array_fill(0, sizeof($singles_filenames), 1748), array_fill(0, sizeof($singles_filenames), 2402));
                 $this->zippingService->zipSession($shooting->getCode());
             } catch (Exception $exception) {
                 $this->deleteFilesShooting($shooting);

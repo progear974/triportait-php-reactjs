107821ba4c584485f093823a7927df63d1b31695
Bax France <baxfrance@hotmail.fr>
2023-02-10 01:14:30 +0100

    update

--- public/images/test.php
+++ public/images/test.php
@@ -15,4 +15,4 @@ function resizeSingle($pathImage, $new_width=1748, $new_height=2402)
 
 $singles_filenames = ["20230210_005120_756.jpg", "20230210_005128_272.jpg", "20230210_005135_710.jpg",
     "20230210_005143_211.jpg", "20230210_005148_118.jpg"];
-array_map(array($this, 'resizeSingle'), $singles_filenames, array_fill(0, sizeof($singles_filenames), 1748), array_fill(0, sizeof($singles_filenames), 2402));
+array_map('resizeSingle', $singles_filenames, array_fill(0, sizeof($singles_filenames), 1748), array_fill(0, sizeof($singles_filenames), 2402));

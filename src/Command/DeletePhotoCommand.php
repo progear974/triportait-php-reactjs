8b99b7adcdfdb176413f8564bc0549c92d1b1418
Bax France <baxfrance@hotmail.fr>
2023-02-13 19:06:19 +0100

    update

--- src/Command/DeletePhotoCommand.php
+++ src/Command/DeletePhotoCommand.php
@@ -44,7 +44,7 @@ class DeletePhotoCommand extends Command
             $io->error("File $filename not found");
             return Command::FAILURE;
         }
-        $codes = explode("\n", file_get_contents($filename));
+        $codes = explode("\n", file_get_contents($filename), -1);
         print_r($codes);
         foreach ($codes as $code) {
             try {
